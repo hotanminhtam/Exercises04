3fd2ee75842cf1672c11efce54d5092e47c1eae7
hotanminhtam <hotanminhtam1703@gmail.com>
2019-09-26 17:47:06 +0700

    Exs04

--- Exercises04/Exercises04/Program.cs
+++ Exercises04/Exercises04/Program.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace Exercises04
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Everybody");
+        }
+
+        public static bool IsOdd(int n)
+        {
+            if(n % 2 !=0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
