2897b987e4317e7a393ac6f29ba65f0931a7b6f0
hotanminhtam <hotanminhtam1703@gmail.com>
2019-10-01 07:47:18 +0700

    ok

--- Exercises04/Exercises04/Program.cs
+++ Exercises04/Exercises04/Program.cs
@@ -21,5 +21,28 @@ namespace Exercises04
             }
             return false;
         }
+
+        public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsPrime(int n)
+        {
+            if (n <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            return true;
+        }
     }
 }
