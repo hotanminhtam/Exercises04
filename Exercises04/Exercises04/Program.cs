51738e8aab2a25492f88acdfb61db13080c957f8
hotanminhtam <hotanminhtam1703@gmail.com>
2019-10-03 06:03:31 +0700

    ok

--- Exercises04/Exercises04/Program.cs
+++ Exercises04/Exercises04/Program.cs
@@ -10,7 +10,17 @@ namespace Exercises04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Everybody");
+            //Console.WriteLine("Hello Everybody");
+            //Console.WriteLine(IsOdd(3));
+            //Console.WriteLine(IsEven(4));
+            //Console.WriteLine(IsPrime(8));
+            //Console.WriteLine(Square(5));
+            //Console.WriteLine(Cube(4));
+            //Console.WriteLine(Factorial(4));
+            //Console.WriteLine(Abs(-1));
+            //Console.WriteLine(Rand1());
+            //Console.WriteLine(Rand2());
+            //Console.WriteLine(Ceil());
         }
 
         public static bool IsOdd(int n)
@@ -62,5 +72,39 @@ namespace Exercises04
                 rs = rs * x;
             return rs;
         }
+
+        public static long Factorial(long n)
+        {
+            int f = 1;
+            for (int i = 2; i <= n; i++)
+                f = f * 1;
+            return f;
+        }
+
+        public static int Abs(int n)
+        {
+            if (n < 0)
+                n = n * (-1);
+            return n;
+        }
+
+        public static int Rand1()
+        {
+            Random rd = new Random();
+            int num = rd.Next();
+            return num;
+        }
+
+        //public static float Rand2()
+        //{
+            //Random rd = new Random();
+            //float num = 
+            //return num;
+        //}
+
+        //public static int Ceil(float x)
+        //{
+            
+        //}
     }
 }
